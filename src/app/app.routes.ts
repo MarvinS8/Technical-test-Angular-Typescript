6f26cd568c049d5ac58eed9010dc3d64d751d4b4
MarvinS8 <marvinsaid1803@gmail.com>
2026-05-15 14:17:50 -0600

    Configure Angular Router with lazy-loaded routes and SSR render modes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home').then(m => m.HomeComponent)
+  },
+  {
+    path: 'countries',
+    loadComponent: () =>
+      import('./pages/countries/countries').then(m => m.CountriesComponent)
+  },
+  {
+    path: 'countries/:code',
+    loadComponent: () =>
+      import('./pages/country-detail/country-detail').then(m => m.CountryDetailComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact').then(m => m.ContactComponent)
+  },
+  // Redirigir rutas no encontradas al home
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { Registro } from './Registro/registro';
-import { APIComponent } from './API-web/API';
-
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [Registro,APIComponent],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Home');
+  title = 'WorldExplorer';
 }
-
